faa55665cfe7cd11e130e734a8acb0b4926ee865
buzzdecafe <m_hur@yahoo.com>
2014-05-25 14:08:53 -0400

    works with my Maybe functor

--- scripts/Maybe.js
+++ scripts/Maybe.js
@@ -1,1 +1,25 @@
-maybe.js
+define([], function() {
+  'use strict';
+  function isNil(x) {
+    return x === null || x === undefined || isNaN(x);
+  }
+
+  function Maybe(x) {
+    this.value = x;
+  }
+
+  Maybe.of = function(x) {
+    return new Maybe(x);
+  };
+
+  Maybe.prototype.map = function(f) {
+    return isNil(this.value) ? this : new Maybe(f(this.value));
+  };
+
+  Maybe.prototype.of = Maybe.of;
+
+  return Maybe.of;
+
+
+});
+
